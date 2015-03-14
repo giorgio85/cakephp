2b99f0fd1dc4d59c5e7ee3ca3413361779bed4a2
Giorgio <Giorgio@Giorgio-PC>
2015-03-14 11:24:47 +0000

    YaEligeCoatingsSegunBase

--- app/Controller/CoatingsController.php
+++ app/Controller/CoatingsController.php
@@ -7,13 +7,26 @@
  */
 
 class CoatingsController extends AppController {
+
     public $helpers = ['Html', 'Form'];
-    
-    public function index(){
+
+    public function index() {
         $this->set('coatings', $this->Coating->find('all'));
     }
-    
-    public function select(){
-        $this->set('coatings', $this->Coating->find('all'));
+
+    public function select($arg1, $arg2, $arg3) {
+        if ($arg1 == 5) {
+            $conditions = ['Coating.compatible' => 5];
+            $this->set('coatings', $this->Coating->find('all', ['conditions' => $conditions]));
+        } else {
+            if ($arg1 == 3 || $arg1 == 4) {
+                $conditions = ['Coating.compatible' => 0];
+                $this->set('coatings', $this->Coating->find('all', ['conditions' => $conditions]));
+            } else {
+                $conditions = ['Coating.compatible' => null];
+                $this->set('coatings', $this->Coating->find('all', ['conditions' => $conditions]));
+            }
+        }
     }
+
 }
